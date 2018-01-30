43099268898a96364d19a705834d18b3e3d22d51
Freewind <pli@thoughtworks.com>
2018-01-30 20:25:20 +0800

    simple buttons

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-ScalaFX Hover Demo
-====================
+ScalaFX Grid Layout Demo
+========================
 
-Open it in IDEA, and run, you will see a window, and when you hover on the Green block it will turn to red.
-
-This code is copied from <http://www.scalafx.org/>
--- src/main/scala/my/Hello.scala
+++ src/main/scala/my/Hello.scala
@@ -4,15 +4,20 @@ import scalafx.application
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.control.Label
+import scalafx.scene.layout.GridPane
 
 object Hello extends JFXApp {
 
   stage = new application.JFXApp.PrimaryStage {
-    title.value = "Hello"
+    title.value = "Grid Layout Demo"
     width = 600
     height = 400
     scene = new Scene {
-      content = new Label("Hello, JavaFX")
+      content = new GridPane {
+        add(new Label("Label1"), 0, 0, 1, 1)
+        add(new Label("Label2"), 0, 1, 2, 2)
+        add(new Label("Label3"), 3, 0, 4, 4)
+      }
     }
   }
 }
